f38c61054eeda7582e063c1db6129612cf4624cd
LThanh <lthanh@netcompany.com>
2022-10-27 13:40:21 +0200

    page for booking

---
+++ src/components/Controls/Checkbox.js
@@ -0,0 +1,36 @@
+import {
+    KeyboardDatePicker,
+    MuiPickersUtilsProvider,
+  } from "@material-ui/pickers";
+  import DateFnsUtils from "@date-io/date-fns";
+  import React from "react";
+  
+  function Checkbox(props) {
+    const { name, label, value, onChange, ...other } = props;
+  
+    const convertToDefEventPara = (name, value) => ({
+      target: {
+        name,
+        value,
+      },
+    });
+  
+    return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          inputVariant="outlined"
+          label={label}
+          format="dd/MM/yyyy"
+          name={name}
+          value={value}
+          {...other}
+          onChange={(date) => onChange(convertToDefEventPara(name, date))}
+        />
+      </MuiPickersUtilsProvider>
+    );
+  }
+  
+  export default Checkbox;
+  
