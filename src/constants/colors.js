b3f0ccee987953b6d1cd26e21633e6df280bd2cc
NCLAN\podk <krzysztof453vip@gmail.com>
2022-10-27 14:58:02 +0200

    Create colors.js
    Added color constants to project.

---
+++ src/constants/colors.js
@@ -0,0 +1,18 @@
+
+
+const systemWhite = "#FFFFFF"
+const systemBlack = "#000000"
+const secondary = "#F8C8DC"
+const primary = "#FF87BA"
+const primaryButton = "#76C8E3"
+const secondaryButton = "#43b4d9"
+
+
+export {
+  systemWhite,
+  secondary,
+  primary,
+  secondaryButton,
+  primaryButton,
+  systemBlack
+}
