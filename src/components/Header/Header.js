f6736c0fa9e0161230875b792904529ffc0401e9
NCLAN\podk <krzysztof453vip@gmail.com>
2022-10-27 15:00:17 +0200

    Update Header.js
    Detailed Header Component.
    TODO:
    Fix static value of header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,27 @@
 import {
-    AppBar,
-    makeStyles,
-    Toolbar,
-  } from "@material-ui/core";
-  import React from "react";
-  
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      backgroundColor: "#FF87BA",
-      height: "107px",
-    },
-  }));
-  
-  function Header() {
-    const classes = useStyles();
-  
-    return (
-      <AppBar position="fixed" className={classes.root} elevation={0}>
-        <Toolbar className={classes.toolbar} variant="dense">
-        </Toolbar>
-      </AppBar>
-    );
-  }
-  
-  export default Header;
-  
+  AppBar,
+  makeStyles,
+  Toolbar,
+} from "@material-ui/core";
+import React from "react";
+import { primary } from '../../constants/colors'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: primary,
+    height: "107px",
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="fixed" className={classes.root} elevation={0}>
+      <Toolbar className={classes.toolbar} variant="dense">
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
