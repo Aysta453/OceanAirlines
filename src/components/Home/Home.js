8276c2340b5932a914c9f34744752830e1b6636c
LThanh <lthanh@netcompany.com>
2022-10-27 10:42:33 +0200

    abc

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,24 +4,42 @@ import {
   import { Link, useNavigate } from "react-router-dom";
   import React, { useState } from "react";
   import { getReduxUser } from "../../utils/Common";
+import Controls from "../Controls/Controls";
   
   const useStyles = makeStyles((theme) => ({    
     root: {
       backgroundColor: '#F8C8DC',
       height: "70vh",
     },
+    mid: {
+      display: "flex",
+      a
+    },
+    typoname: {
+      fontSize: "100px",
+      fontWeight: "600",
+      textAlign: "center",
+      color: "#fff",
+    }
   }));
   
   function Home() {
     const classes = useStyles();
     return (
       <div className={classes.root}>
-        <Typography
-                variant="subtitle1"
-                component="div"
+        <div className={classes.mid}>
+          <Typography
+                variant="h1"
+                component="h1"
                 color="secondary"
                 className={classes.typoname}
-              ></Typography>
+              >Oceanic World Airlines</Typography>
+             <Controls.Button
+              className={classes.btn}
+              type="submit"
+              text="Sign In"
+            />
+        </div>
       </div>
     );
   }
